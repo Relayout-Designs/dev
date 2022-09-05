6547200d2f5ddb65f5d8de7b800f71efd5526708
Vijay krishna <vijaywaskrishna@gmail.com>
2022-09-06 00:26:42 +0530

    Added Grid System

---
+++ sample.js
@@ -0,0 +1,32 @@
+function double(number) {
+    return number * 3
+}
+
+double(2)
+double(5)
+
+//fucnctions sample
+function add(a, b) {
+    return a + b
+}
+
+//fucnctions sample
+function double(number) {
+    return number * 3
+}
+
+let firstNumber = double(2)
+let secondNumber = double(4)
+
+
+//fucnctions sample
+let name = "vijay"
+
+console.log(name)
+
+
+//fucnctions sample
+function double(number) {
+    console.log(number * 2)
+    return number * 2
+}
